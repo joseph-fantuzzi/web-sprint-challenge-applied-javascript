0f213e76b6b159a5e2c63a024dcfdce184e1128a
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-02-11 11:43:18 -0500

    completed the tabsAppender function for task 4

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -35,6 +37,19 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+
+  axios
+    .get("http://localhost:5000/api/topics")
+    .then((resp) => {
+      const tabElement = document.querySelector(selector);
+      tabElement.appendChild(Tabs(resp.data.topics));
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      console.log("DONE");
+    });
 };
 
 export { Tabs, tabsAppender };
